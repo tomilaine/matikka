8655a9969755deb433faee58098fea386626e30f
Tomi Laine <tomi.laine@relex.fi>
2018-09-17 19:31:35 +0300

    refactor and make multiplication work as well

--- matikka.rb
+++ matikka.rb
@@ -3,6 +3,7 @@
 LOWEST_NUMBER = 1
 HIGHEST_NUMBER = 10
 
+QUESTIONS = 10
 PENALTY = 20
 
 $score = 0
@@ -25,63 +26,53 @@ def multiplication_table
   number_pairs.map{ |number_pair| number_pair << number_pair[0] * number_pair[1] }
 end
 
-def shuffled_multiplication_table()
-  multiplication_table.shuffle[0..9]
+def shuffled_multiplication_table
+  multiplication_table.shuffle[0..QUESTIONS-1]
 end
 
 def multiplication
   table = shuffled_multiplication_table
-  table.each do |row|
-    answer = ask("Mitä on #{row[0]} * #{row[1]}?")
-    if answer == row[2]
-      puts "AIKA PRO!"
-    else
-      puts "lisää harjoitusta..."
-    end
-    puts "--------------------------------"
-  end
+  table.each { |row| ask("#{row[0]} * #{row[1]}", row[2]) }
 end
 
 def division
-  start_time = Time.now
   table = shuffled_multiplication_table
-  table.each do |row|
-    puts "--------------------------------"
-    answer = ask("#{row[2]} : #{row[1]} = ?")
-    if answer == row[0]
-      puts "AIKA PRO!"
-      $score = $score + 1
-    else
-      puts "lisää harjoitusta... Se oli #{row[0]}"
-      $fails << ["#{row[2]} : #{row[1]} = #{row[0]}"]
-
-    end
-    puts "Score: #{$score}"
-    sleep 0.5
-  end
-  end_time = Time.now
-  $time_score = (end_time - start_time).round
+  table.each { |row| ask("#{row[2]} : #{row[1]}", row[0]) }
 end
 
-def ask(question)
-  puts question
-  gets.chomp.to_i
+def ask(question, answer)
+  puts "--------------------------------"
+  puts question + " = ?"
+  if answer == gets.chomp.to_i
+    puts "AIKA PRO!"
+    $score = $score + 1
+    puts "Score: #{$score}"
+  else
+    puts "lisää harjoitusta... Se oli #{answer}"
+    $fails << "#{question} = #{answer}"
+  end
+  sleep 0.5
 end
 
 puts "1. Kertolaskuja"
 puts "2. Jakolaskuja"
 print "Mitäs laitetaan? "
 selection = gets.chomp.to_i
+
+start_time = Time.now
 if selection == 1
   multiplication
 elsif selection == 2
   division
 else
   puts "No ei sit! 💩"
+  exit
 end
+end_time = Time.now
 
+time_score = (end_time - start_time).round
 penalty_time = $fails.count * PENALTY
-final_score = $time_score + penalty_time
+final_score = time_score + penalty_time
 title = score_title(final_score)
 
 puts "\n--------------------------------"
